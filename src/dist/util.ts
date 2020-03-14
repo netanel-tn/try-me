8a38f7e56438c1527f13bd4a42b615fee3d8350a
Netanel Tal Nizri <netanel0058@gmail.com>
2020-03-14 16:15:08 +0200

    Update .gitignore

---
+++ src/dist/util.ts
@@ -0,0 +1,11 @@
+import 'colors';
+
+/**
+  * Method Name
+  */
+export const metName = (name: string) => '\x1b[34m Method \x1b[0m`' + name + '`';
+
+/**
+  * Whether Had an Error
+  */
+export type HadErr = null | any;
